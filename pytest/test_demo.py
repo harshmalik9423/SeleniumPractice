c7168a3d558f2296c473dc17b9b7c1556a954cca
Harsh Malik <harshmalik2013@gmail.com>
2020-08-28 18:34:34 +0530

    pytest demo added

---
+++ pytest/test_demo.py
@@ -0,0 +1,11 @@
+import pytest
+
+def testMethod1():
+    print("This is test Method 1")
+
+def testMethod2():
+    print("This is test Method 2")
+
+def testMethod3():
+    print("This is test Method 3")
+
